27aacd5b1c696923c60f243c7f480b8e5c28ae99
pinkdeer <bunman@mail.ru>
2019-04-12 15:42:45 +0300

    Edited blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def posts_list(request):
     n = ['Tom', 'Jack', 'Tim']
     return render(request, 'blog/index.html', context={'names': n})
