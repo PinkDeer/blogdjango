d47618b0a2c0c54bacdc536e996207abddf8c395
pinkdeer <bunman@mail.ru>
2019-05-23 13:46:17 +0300

    Add vavlidations and optimization

--- blog/forms.py
+++ blog/forms.py
@@ -2,20 +2,35 @@ from django import forms
 from .models import Tag
 from django.core.exceptions import ValidationError
 
-class TagForm(forms.Form):
-        title = forms.CharField(max_length=50)
-        slug = forms.CharField(max_length=50)
+class TagForm(forms.ModelForm):
+    # title = forms.CharField(max_length=50)
+    # slug = forms.CharField(max_length=50)
+    #
+    # title.widget.attrs.update({'class': 'form-control'})
+    # slug.widget.attrs.update({'class': 'form-control'})
 
-        def clean_slug(self):
-            new_slug = self.cleaned_date['slug'].lower()
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
 
-            if new_slug == 'create':
-                raise ValidationError('Slug my not be "create"')
-            return new_slug
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
-        def save(self):
-            new_tag = Tag.objects.create(
-                title=self.cleaned_data['title'],
-                slug=self.cleaned_data['slug']
-            )
-            return new_tag
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug my not be "create"')
+        if Tag.objects.filter(slug__iexact=new_slug).count():
+            raise ValidationError('Slug must be unique. We have "{}" slug already'.format(new_slug))
+        return new_slug
+
+    # def save(self):
+    #     new_tag = Tag.objects.create(
+    #         title=self.cleaned_data['title'],
+    #         slug=self.cleaned_data['slug']
+    #     )
+    #     return new_tag
